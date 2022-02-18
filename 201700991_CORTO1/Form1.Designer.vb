f7550b6440ad059518f7248aad6a84b0c9b7de7c
JnthnLnrs <98726378+JnthnLnrs@users.noreply.github.com>
2022-02-17 19:12:09 -0600

    Corto1

---
+++ 201700991_CORTO1/Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtMonto = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.rbNorte = New System.Windows.Forms.RadioButton()
+        Me.rbSur = New System.Windows.Forms.RadioButton()
+        Me.rbOriente = New System.Windows.Forms.RadioButton()
+        Me.rbOccidente = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.rbAccidente = New System.Windows.Forms.RadioButton()
+        Me.rbVehiculo = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(61, 31)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(231, 20)
+        Me.txtNombre.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(11, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Monto"
+        '
+        'txtMonto
+        '
+        Me.txtMonto.Location = New System.Drawing.Point(61, 72)
+        Me.txtMonto.Name = "txtMonto"
+        Me.txtMonto.Size = New System.Drawing.Size(100, 20)
+        Me.txtMonto.TabIndex = 5
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.GroupBox3)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.txtNombre)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtMonto)
+        Me.GroupBox1.Location = New System.Drawing.Point(154, 23)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(304, 248)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingresos"
+        '
+        'rbNorte
+        '
+        Me.rbNorte.AutoSize = True
+        Me.rbNorte.Location = New System.Drawing.Point(9, 19)
+        Me.rbNorte.Name = "rbNorte"
+        Me.rbNorte.Size = New System.Drawing.Size(51, 17)
+        Me.rbNorte.TabIndex = 0
+        Me.rbNorte.TabStop = True
+        Me.rbNorte.Text = "Norte"
+        Me.rbNorte.UseVisualStyleBackColor = True
+        '
+        'rbSur
+        '
+        Me.rbSur.AutoSize = True
+        Me.rbSur.Location = New System.Drawing.Point(9, 42)
+        Me.rbSur.Name = "rbSur"
+        Me.rbSur.Size = New System.Drawing.Size(41, 17)
+        Me.rbSur.TabIndex = 1
+        Me.rbSur.TabStop = True
+        Me.rbSur.Text = "Sur"
+        Me.rbSur.UseVisualStyleBackColor = True
+        '
+        'rbOriente
+        '
+        Me.rbOriente.AutoSize = True
+        Me.rbOriente.Location = New System.Drawing.Point(9, 65)
+        Me.rbOriente.Name = "rbOriente"
+        Me.rbOriente.Size = New System.Drawing.Size(59, 17)
+        Me.rbOriente.TabIndex = 2
+        Me.rbOriente.TabStop = True
+        Me.rbOriente.Text = "Oriente"
+        Me.rbOriente.UseVisualStyleBackColor = True
+        '
+        'rbOccidente
+        '
+        Me.rbOccidente.AutoSize = True
+        Me.rbOccidente.Location = New System.Drawing.Point(9, 88)
+        Me.rbOccidente.Name = "rbOccidente"
+        Me.rbOccidente.Size = New System.Drawing.Size(74, 17)
+        Me.rbOccidente.TabIndex = 3
+        Me.rbOccidente.TabStop = True
+        Me.rbOccidente.Text = "Occidente"
+        Me.rbOccidente.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.rbNorte)
+        Me.GroupBox2.Controls.Add(Me.rbOccidente)
+        Me.GroupBox2.Controls.Add(Me.rbSur)
+        Me.GroupBox2.Controls.Add(Me.rbOriente)
+        Me.GroupBox2.Location = New System.Drawing.Point(14, 115)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(118, 111)
+        Me.GroupBox2.TabIndex = 7
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Region"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.rbAccidente)
+        Me.GroupBox3.Controls.Add(Me.rbVehiculo)
+        Me.GroupBox3.Location = New System.Drawing.Point(148, 121)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(144, 105)
+        Me.GroupBox3.TabIndex = 8
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Seguro"
+        '
+        'rbAccidente
+        '
+        Me.rbAccidente.AutoSize = True
+        Me.rbAccidente.Location = New System.Drawing.Point(6, 42)
+        Me.rbAccidente.Name = "rbAccidente"
+        Me.rbAccidente.Size = New System.Drawing.Size(73, 17)
+        Me.rbAccidente.TabIndex = 1
+        Me.rbAccidente.TabStop = True
+        Me.rbAccidente.Text = "Accidente"
+        Me.rbAccidente.UseVisualStyleBackColor = True
+        '
+        'rbVehiculo
+        '
+        Me.rbVehiculo.AutoSize = True
+        Me.rbVehiculo.Location = New System.Drawing.Point(6, 19)
+        Me.rbVehiculo.Name = "rbVehiculo"
+        Me.rbVehiculo.Size = New System.Drawing.Size(66, 17)
+        Me.rbVehiculo.TabIndex = 0
+        Me.rbVehiculo.TabStop = True
+        Me.rbVehiculo.Text = "Vehiculo"
+        Me.rbVehiculo.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(154, 284)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(266, 284)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Limpiar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(383, 284)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Salir"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Label4)
+        Me.GroupBox4.Controls.Add(Me.Label3)
+        Me.GroupBox4.Controls.Add(Me.TextBox4)
+        Me.GroupBox4.Controls.Add(Me.TextBox3)
+        Me.GroupBox4.Location = New System.Drawing.Point(464, 32)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(241, 103)
+        Me.GroupBox4.TabIndex = 10
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Resultados"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(119, 22)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 0
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(119, 60)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(19, 33)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Seguro"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(19, 63)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Sueldo"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(717, 340)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "201700991_Envios"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtMonto As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents rbNorte As System.Windows.Forms.RadioButton
+    Friend WithEvents rbSur As System.Windows.Forms.RadioButton
+    Friend WithEvents rbOriente As System.Windows.Forms.RadioButton
+    Friend WithEvents rbOccidente As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents rbAccidente As System.Windows.Forms.RadioButton
+    Friend WithEvents rbVehiculo As System.Windows.Forms.RadioButton
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ 201700991_CORTO1/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+
+    Const viaticosNorte As Double = 150
+    Const viaticosSur As Double = 200
+    Const viaticosOriente As Double = 175
+    Const viaticosOccidente As Double = 225
+
+    Const seguroVehiculo As Double = 3 / 100
+    Const seguroAccidente As Double = 5 / 100
+
+    Const sueldoBase As Double = 3800
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+
+
+
+
+
+       
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+
+
+        txtNombre.Clear()
+        txtMonto.Clear()
+
+        txtNombre.Focus()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        End
+
+    End Sub
+End Class
